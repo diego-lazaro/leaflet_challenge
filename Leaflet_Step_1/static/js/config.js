340ab1d2ed6c0fa6c985f23fb64ec6b33be5ad59
diego-lazaro <115186079+diego-lazaro@users.noreply.github.com>
2023-04-02 11:05:23 -0700

    update

--- Leaflet_Step_1/static/js/config.js
+++ Leaflet_Step_1/static/js/config.js
@@ -1,2 +1,4 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiamNvbmNoYTU2NDI5IiwiYSI6ImNrcXN0N2VkczIyNWQyb3Fxcnc2dWZmZXYifQ.TBSwlgogDIMZ686A1bNFEw";
+// const API_KEY = "pk.eyJ1IjoiamNvbmNoYTU2NDI5IiwiYSI6ImNrcXN0N2VkczIyNWQyb3Fxcnc2dWZmZXYifQ.TBSwlgogDIMZ686A1bNFEw";
+
+const API_KEY = ""pk.eyJ1Ijoicm9iaW53YXZlcyIsImEiOiJja3FzdDMwNTQxeWMxMnVtenpsdHdvZmxsIn0.1AkDF0OBJ56UVfkM5K_UxA";
